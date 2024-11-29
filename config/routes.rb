a64d02b79d7e1943d69cdb76ca72ace63309931d
matthewgretton <matthew.gretton@gmail.com>
2024-11-29 23:13:28 +0000

    Added code in order to make the app a PWA

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,13 @@ Rails.application.routes.draw do
   get "books/search", to: "books#search", as: "search_books"
   post "books/search_by_isbns", to: "books#search_by_isbns", as: "search_by_isbns_books"
 
+  # Uncomment this line for PWA support
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # We don't need the service-worker route since we're not implementing offline functionality
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
+  get "up" => "rails/health#show", as: :rails_health_check
+
   root "books#search"
 end
