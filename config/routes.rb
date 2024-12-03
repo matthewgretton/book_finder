e8e5d7e8781f10390d5f71ce9fd9d1db16cb2af1
matthewgretton <matthew.gretton@gmail.com>
2024-12-03 13:12:32 +0000

    Making detailed view the default

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "books#search"
+
+  Rails.application.routes.default_url_options[:detailed] = true
 end
