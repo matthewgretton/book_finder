194ba830d71133c139dd3b18f32b2793b475af3d
matthewgretton <matthew.gretton@gmail.com>
2024-12-19 23:44:36 +0000

    it should now be possible to take photos of multiple barcodes

--- app/javascript/controllers/file_upload_controller.js
+++ app/javascript/controllers/file_upload_controller.js
@@ -1,15 +1,21 @@
-// app/javascript/controllers/file_upload_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["fileInput"]
+  static targets = ["fileInput", "submitButton"]
 
   promptFileSelection(event) {
     event.preventDefault()
     this.fileInputTarget.click()
   }
 
-  submitForm() {
+  handleFileChange() {
+    if (this.fileInputTarget.files.length > 0) {
+      this.submitButtonTarget.classList.remove("hidden")
+    }
+  }
+
+  submitForm(event) {
+    event.preventDefault()
     this.element.requestSubmit()
   }
 }
